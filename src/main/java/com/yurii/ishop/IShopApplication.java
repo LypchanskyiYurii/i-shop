d6b7ce74330533165b09c233e88a79491d0ef7b6
Yurii <yurii.lypchanskyi@gmail.com>
2021-11-23 00:16:47 +0200

    Fixed

--- src/main/java/com/yurii/ishop/IShopApplication.java
+++ src/main/java/com/yurii/ishop/IShopApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IShopApplication {
 
-
+	// This App don't work
 	public static void main(String[] args) {
 		SpringApplication.run(IShopApplication.class, args);
 	}
